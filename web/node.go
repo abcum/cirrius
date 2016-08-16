5f966526a50c23f68daf511282d6a9f982781a01
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-08-16 11:41:45 +0100

    Add react npm package

--- web/node.go
+++ web/node.go
@@ -57,6 +57,7 @@ import (
 	_ "github.com/abcum/cirrius/npm/modules/promiz"
 	_ "github.com/abcum/cirrius/npm/modules/q"
 	_ "github.com/abcum/cirrius/npm/modules/qs"
+	_ "github.com/abcum/cirrius/npm/modules/react"
 	_ "github.com/abcum/cirrius/npm/modules/rx"
 	_ "github.com/abcum/cirrius/npm/modules/semver"
 	_ "github.com/abcum/cirrius/npm/modules/shortid"
