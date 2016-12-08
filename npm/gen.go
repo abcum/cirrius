5ac098d9bc856b5f75e4affca005b9fabf6357ef
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-12-08 22:03:12 +0000

    Place build instruction before package name

--- npm/gen.go
+++ npm/gen.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package npm
-
 //+build generate
+
+package npm
 
 //go:generate go-bindata -pkg npm -ignore=.go -o npm.gen.go ./...
