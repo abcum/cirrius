d1d807acdd1271f8bcffda06e176421c3c211d7f
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-03-09 12:37:25 +0000

    Remove old node npm modules

--- build/fmt.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-total=`git ls-files | grep '.go$' | xargs gofmt -l 2>&1 | wc -l`
-
-if [ $total -gt 0 ]; then
-    echo "run 'go fmt ./...' to format your source code."
-    exit 1
-fi
